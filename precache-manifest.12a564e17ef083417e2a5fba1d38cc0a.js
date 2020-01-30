bcb2fb1ac501283ba73d0c20fe2d5bd5d1ed0007
Paolo Soncco <paholo2054@gmail.com>
2020-01-29 20:49:28 -0500

    asdfasdfsad

---
+++ precache-manifest.12a564e17ef083417e2a5fba1d38cc0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d339cedfe66cdc3c4c4dca8fdc12d3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "952533bd72fdd1cea4af",
+    "url": "/static/css/main.a95c0c2c.chunk.css"
+  },
+  {
+    "revision": "2de6f0f92cc94e29a677",
+    "url": "/static/js/2.39cf05b7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.39cf05b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "952533bd72fdd1cea4af",
+    "url": "/static/js/main.fb67a64d.chunk.js"
+  },
+  {
+    "revision": "b9b1c4adac5ff6076b5c",
+    "url": "/static/js/runtime-main.fbde1115.js"
+  },
+  {
+    "revision": "c52c69467d157c2248480b66a5a2b4cf",
+    "url": "/static/media/calavera.c52c6946.jpg"
+  }
+]);
